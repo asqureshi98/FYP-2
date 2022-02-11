4ba82f56a3d94231bbe4adc8df3c73b19afde266
Abdul Samad Qureshi <68150729+asqureshi98@users.noreply.github.com>
2022-02-11 10:52:25 +0500

    theme added to main file

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'booking_confirmation.dart';
 import 'artists.dart';
 import 'artistdetails.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -23,11 +24,44 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Experts(),
-        )],
+        )
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.pink,
+          accentColor: Colors.blue,
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink,
+                ),
+                headline1: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+                button: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                bodyText1: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  headline5: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+          ),
         ),
         home: artistdetails(),
       ),
